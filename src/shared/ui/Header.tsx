6f1ccc837507cfcc2654a507f263926cc131b4a4
김도예 <kmdy125@gmail.com>
2025-05-16 21:25:59 +0900

    refactor: 아이콘 import 구조 개선 및 withIconSize 유틸 추가

--- src/shared/ui/Header.tsx
+++ src/shared/ui/Header.tsx
@@ -1,11 +1,12 @@
-import DarkModeIcon from '../assets/icons/darkmode.svg';
-import LightModeIcon from '../assets/icons/lightmode.svg';
-import NotiIcon from '../assets/icons/noti.svg';
-import UserIcon from '../assets/icons/user.svg';
+import {
+  DarkModeIcon,
+  LightModeIcon,
+  NotiIcon,
+  UserIcon,
+} from '../assets/icons';
+
 import * as S from './Header.styles';
 import { colors } from '../constants';
-
-const ICON_SIZE = 24;
 
 // zustand로 관리 예정
 const isLogin = true;
@@ -17,30 +18,18 @@ const Header = () => {
       <S.Logo src='/logo.svg' alt='Logo' />
       <S.RightSection>
         {mode == 'dark' ? (
-          <DarkModeIcon
-            width={ICON_SIZE}
-            height={ICON_SIZE}
-            fill={colors.dark.PRIMARY}
-          />
+          <DarkModeIcon fill={colors.dark.PRIMARY} />
         ) : (
-          <LightModeIcon
-            width={ICON_SIZE}
-            height={ICON_SIZE}
-            fill={colors.dark.PRIMARY}
-          />
+          <LightModeIcon fill={colors.dark.PRIMARY} />
         )}
         {isLogin ? (
           <S.IconGroup>
             <UserIcon
-              width={ICON_SIZE}
-              height={ICON_SIZE}
               stroke={mode === 'dark' ? colors.dark.BLACK : colors.light.BLACK}
               strokeWidth={2}
               fill='none'
             />
             <NotiIcon
-              width={ICON_SIZE}
-              height={ICON_SIZE}
               stroke={mode === 'dark' ? colors.dark.BLACK : colors.light.BLACK}
               strokeWidth={2}
               fill='none'
