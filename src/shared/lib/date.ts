6a4050be145874de520e4d6144e4a7f0647029f6
김도예 <kmdy125@gmail.com>
2025-05-29 17:37:27 +0900

    feat: n분 전 등 상대 시간 변환 함수 추가

---
+++ src/shared/lib/date.ts
@@ -0,0 +1,28 @@
+export function convertDateToString(dateString: string) {
+  const dateTime = new Date(dateString);
+  const now = new Date();
+  console.log(dateString, 'datetime', dateTime, 'now', now);
+
+  const diff = now.getTime() - dateTime.getTime();
+
+  const minutes = Math.floor(diff / (1000 * 60));
+  const hours = Math.floor(diff / (1000 * 60 * 60));
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const weeks = Math.floor(diff / (1000 * 60 * 60 * 24 * 7));
+  const months = Math.floor(diff / (1000 * 60 * 60 * 24 * 30));
+  const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
+
+  if (years === 0 && months === 0 && weeks === 0 && days === 0 && hours === 0) {
+    return `${minutes}분 전`;
+  } else if (years === 0 && months === 0 && weeks === 0 && days === 0) {
+    return `${hours}시간 전`;
+  } else if (years === 0 && months === 0 && weeks === 0) {
+    return `${days}일 전`;
+  } else if (years === 0 && months === 0) {
+    return `${weeks}주 전`;
+  } else if (years === 0) {
+    return `${months}달 전`;
+  } else {
+    return `${years}년 전`;
+  }
+}
