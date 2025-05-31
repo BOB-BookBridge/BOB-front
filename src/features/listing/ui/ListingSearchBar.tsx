3e8fb75ce3a08f36e65691a263ec08a03626c704
김도예 <kmdy125@gmail.com>
2025-05-31 18:15:39 +0900

    feat: 검색 키 드롭다운 구현

--- src/features/listing/ui/ListingSearchBar.tsx
+++ src/features/listing/ui/ListingSearchBar.tsx
@@ -1,17 +1,47 @@
 'use client';
 
+import { useState } from 'react';
 import styled, { useTheme } from 'styled-components';
 import { ArrowICon, DropdownIconSm } from '@/shared/assets/icons';
 
+const options = [
+  { value: 'all', label: '통합' },
+  { value: 'title', label: '제목' },
+  { value: 'author', label: '저자' },
+];
+
 const ListingSearchBar = () => {
   const theme = useTheme();
+  const [isOpen, setIsOpen] = useState(false);
+  const [selected, setSelected] = useState('all');
+  function handleDropdownToggle() {
+    setIsOpen((prev) => !prev);
+  }
+  function handleDropdownOptionClick(value: string) {
+    setSelected(value);
+    setIsOpen(false);
+  }
   return (
-    <InputWrapper>
+    <Container>
       <div style={{ display: 'flex' }}>
-        <KeyDropdown>
-          <span>전체</span>
-          <DropdownIconSm style={{ fill: theme.colors.BLACK }} />
-        </KeyDropdown>
+        <DropdownWrapper>
+          <KeyDropdown onClick={handleDropdownToggle}>
+            <span>{options.find((opt) => opt.value === selected)?.label}</span>
+            <DropdownIconSm style={{ fill: theme.colors.BLACK }} />
+          </KeyDropdown>
+          {isOpen && (
+            <DropdownList>
+              {options.map((option) => (
+                <DropdownItem
+                  key={option.value}
+                  selected={option.value === selected}
+                  onClick={() => handleDropdownOptionClick(option.value)}>
+                  {option.label}
+                </DropdownItem>
+              ))}
+            </DropdownList>
+          )}
+        </DropdownWrapper>
         <span
           style={{
             color: theme.colors.GRAY_300,
@@ -22,13 +52,13 @@ const ListingSearchBar = () => {
         <StyledInput placeholder='검색어를 입력하세요' />
       </div>
       <ArrowICon style={{ fill: theme.colors.GRAY_600 }} />
-    </InputWrapper>
+    </Container>
   );
 };
 
 export default ListingSearchBar;
 
-const InputWrapper = styled.div`
+const Container = styled.div`
   border: 1px solid ${({ theme }) => theme.colors.GRAY_300};
   border-radius: 20px;
   height: 40px;
@@ -39,15 +69,49 @@ const InputWrapper = styled.div`
   padding: 10px;
 `;
 
-const KeyDropdown = styled.div`
+const DropdownWrapper = styled.div`
+  position: relative;
+  width: 70px;
   display: flex;
   align-items: center;
   justify-content: center;
   font-size: 14px;
+`;
+
+const KeyDropdown = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   font-weight: 600;
-  width: 70px;
+  width: 100%;
   gap: 5px;
   cursor: pointer;
+`;
+
+const DropdownList = styled.div`
+  position: absolute;
+  width: 100%;
+  top: 100%;
+  margin-top: 10px;
+  left: 0;
+  background-color: ${({ theme }) => theme.colors.WHITE};
+  border-radius: 10px;
+  box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.2);
+  z-index: 10;
+`;
+
+const DropdownItem = styled.div<{ selected: boolean }>`
+  padding: 5px;
+  cursor: pointer;
+  border-radius: 5px;
+  margin: 5px;
+  font-weight: ${({ selected }) => (selected ? 700 : 400)};
+  color: ${({ selected, theme }) =>
+    selected ? theme.colors.BLACK : theme.colors.GRAY_500};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.GRAY_300};
+  }
 `;
 const StyledInput = styled.input`
   border: none;
