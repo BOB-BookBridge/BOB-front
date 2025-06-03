e255c69df034d51b6605226d039ffaf6afff2706
김도예 <kmdy125@gmail.com>
2025-06-03 17:30:12 +0900

    feat: 필터 사이드바 초기화 버튼 추가

--- src/features/listing/ui/FilterSidebar.tsx
+++ src/features/listing/ui/FilterSidebar.tsx
@@ -2,6 +2,7 @@
 import styled from 'styled-components';
 import { useFilterStore } from '../model';
 import FilterContentControl from './FilterContentControl';
+import { colors } from '@/shared/constants';
 
 const FilterSideBar = () => {
   const isAvailableOnly = useFilterStore((state) => state.isAvailableOnly);
@@ -18,7 +19,24 @@ const FilterSideBar = () => {
 
   return (
     <Container>
-      <p style={{ fontSize: 20, fontWeight: 600, margin: 10 }}>필터</p>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'end',
+          padding: 10,
+        }}>
+        <p style={{ fontSize: 20, fontWeight: 600, margin: 0 }}>필터</p>
+        <span
+          onClick={resetFilters}
+          style={{
+            color: colors.light.GRAY_500,
+            textDecoration: 'underline',
+            cursor: 'pointer',
+          }}>
+          초기화
+        </span>
+      </div>
       <FilterContentControl
         isAvailableOnly={isAvailableOnly}
         categoryId={categoryId}
