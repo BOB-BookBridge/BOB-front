cfffa8bc312ec2a3448596e4db007a24feb9759d
김도예 <kmdy125@gmail.com>
2025-06-03 18:35:08 +0900

    feat: 정렬 드롭다운 UI 구현 및 zustand 연동

--- src/features/listing/ui/ListingSortDropdown.tsx
+++ src/features/listing/ui/ListingSortDropdown.tsx
@@ -1,14 +1,95 @@
 'use client';
 
-import styled from 'styled-components';
+import { SortKey } from '@/entities/listing/model/types';
+import { DropdownIconSm } from '@/shared/assets/icons';
+import { sortMap } from '@/shared/lib';
+import { useState } from 'react';
+import styled, { useTheme } from 'styled-components';
+import { useFilterStore } from '../model';
+
+const options: SortKey[] = ['RECENT', 'OLD', 'LOW_PRICE', 'HIGH_PRICE'];
 
 const ListingSortDropdown = () => {
-  return <Container>정렬 자리</Container>;
+  const theme = useTheme();
+  const [isOpen, setIsOpen] = useState(false);
+  const sort = useFilterStore((state) => state.sort);
+  const { setSort } = useFilterStore();
+  function handleDropdownToggle() {
+    setIsOpen((prev) => !prev);
+  }
+  function handleDropdownOptionClick(value: SortKey) {
+    setSort(value);
+    setIsOpen(false);
+  }
+  return (
+    <Container>
+      <DropdownBox onClick={handleDropdownToggle}>
+        <span>{sortMap[options.find((opt) => opt === sort) ?? 'RECENT']}</span>
+
+        <DropdownIconSm style={{ fill: theme.colors.BLACK }} />
+      </DropdownBox>
+      {isOpen && (
+        <DropdownList>
+          {options.map((option) => (
+            <DropdownItem
+              key={option}
+              selected={option === sort}
+              onClick={() => handleDropdownOptionClick(option)}>
+              {sortMap[option]}
+            </DropdownItem>
+          ))}
+        </DropdownList>
+      )}
+    </Container>
+  );
 };
 export default ListingSortDropdown;
 
 const Container = styled.div`
+  position: relative;
   width: 100px;
   height: 40px;
-  background-color: skyblue;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+`;
+
+const DropdownBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 500;
+  width: 100%;
+  gap: 5px;
+  cursor: pointer;
+  padding: 5px 0 5px 5px;
+  border-radius: 10px;
+  border: 1px solid ${({ theme }) => theme.colors.GRAY_400};
+`;
+
+const DropdownList = styled.div`
+  position: absolute;
+  width: 100%;
+  top: 100%;
+  margin-top: 10px;
+  left: 0;
+  background-color: ${({ theme }) => theme.colors.WHITE};
+  border-radius: 10px;
+  box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.2);
+  z-index: 1000;
+`;
+
+const DropdownItem = styled.div<{ selected: boolean }>`
+  padding: 5px;
+  cursor: pointer;
+  border-radius: 5px;
+  margin: 5px;
+  font-weight: ${({ selected }) => (selected ? 500 : 400)};
+  color: ${({ selected, theme }) =>
+    selected ? theme.colors.BLACK : theme.colors.GRAY_500};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.GRAY_300};
+  }
 `;
