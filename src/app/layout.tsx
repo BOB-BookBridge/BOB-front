428ce417405e1768dfa9f20a1d118432f5b69444
김도예 <kmdy125@gmail.com>
2025-06-05 00:17:37 +0900

    chore: 기본 메타데이터 등록 및 favicon 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from 'next/font/local';
 import StyledComponentsRegistry from '@/shared/lib/StyledComponentsRegistry';
 import ThemeRegistry from '@/shared/providers/ThemeRegistry';
 import { Header } from '@/shared/ui';
+import { Metadata } from 'next';
 
 const pretendard = localFont({
   src: '../shared/assets/fonts/PretendardVariable.woff2',
@@ -29,3 +30,11 @@ export default function RootLayout({
     </html>
   );
 }
+
+export const metadata: Metadata = {
+  title: 'BOOK BRIDGE | BOB',
+  description: '중고책 거래를 통해 사람과 책을 잇는 플랫폼, BOOK BRIDGE입니다.',
+  icons: {
+    icon: '/logo.png',
+  },
+};
