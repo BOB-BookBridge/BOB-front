de1d31f696bb0e2694cee039999691bb0e98364b
김도예 <kmdy125@gmail.com>
2025-06-11 20:01:43 +0900

    feat: 마이페이지 - 내 프로필 - 활동 지역 수정 UI 구현

--- src/features/user/ui/EditProfile.styles.ts
+++ src/features/user/ui/EditProfile.styles.ts
@@ -10,3 +10,22 @@ export const NicknameSection = styled.div`
 export const ButtonWrapper = styled.div`
   width: 100px;
 `;
+
+export const EditAreaContainer = styled.div`
+  margin-top: 20px;
+  width: 100%;
+`;
+
+export const AreaInfoBar = styled.div`
+  display: flex;
+  margin-top: 10px;
+  gap: 10px;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const InfoText = styled.div`
+  margin-top: 10px;
+  color: ${({ theme }) => theme.colors.GRAY_600};
+  font-size: 15px;
+`;
--- src/features/user/ui/EditProfile.tsx
+++ src/features/user/ui/EditProfile.tsx
@@ -1,12 +1,14 @@
 import DefaultProfile from '@/shared/assets/default-profile.svg';
 import EditNickname from './EditNickname';
+import EditArea from './EditArea';
 
 const EditProfile = () => {
   // #todo: 기본값 처리
   return (
-    <div style={{ width: '100%', marginTop: 50 }}>
+    <div style={{ width: '100%', marginTop: 50, padding: 10 }}>
       <DefaultProfile width={100} />
       <EditNickname />
+      <EditArea />
     </div>
   );
 };
