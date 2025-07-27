8e325ffb1b92fdfb71dd736b8ce612ddf760070b
김도예 <kmdy125@gmail.com>
2025-07-27 21:15:40 +0900

    chore: test 관련 라이브러리 설치 및 환경 설정

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.mjs$': 'ts-jest',
+  },
+};
+
+export default createJestConfig(customJestConfig);
