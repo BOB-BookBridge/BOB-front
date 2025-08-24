1b4940e61b939001e169d7169fac3c4f02ea861e
김도예 <kmdy125@gmail.com>
2025-08-25 03:46:01 +0900

    feat: 에러페이지에 cause 쿼리 파라미터 표시

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -4,22 +4,29 @@ import Image from 'next/image';
 import styled, { useTheme } from 'styled-components';
 import ErrorLogo from '@/shared/assets/error-logo.png';
 import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
 const ErrorPage = () => {
   const theme = useTheme();
+  const searchParams = useSearchParams();
+  const cause = searchParams?.get('cause');
+
   return (
     <Container>
       <Image src={ErrorLogo.src} width={150} height={150} alt={'logo'} />
       <TextContainer>
-        <div>로그인 과정에서 문제가 발생했습니다</div>
-        <div>다시 진행해 주세요</div>
+        <div style={{ marginBottom: 10 }}>
+          <div>로그인 과정에서 문제가 발생했습니다</div>
+          <div>다시 진행해 주세요</div>
+          {cause && <CauseText>{cause}</CauseText>}
+        </div>
+        <Link
+          href='/login'
+          style={{
+            color: theme.colors.GRAY_500,
+          }}>
+          로그인 페이지로
+        </Link>
       </TextContainer>
-      <Link
-        href='/login'
-        style={{
-          color: theme.colors.GRAY_500,
-        }}>
-        로그인 페이지로
-      </Link>
     </Container>
   );
 };
@@ -39,5 +46,11 @@ const Container = styled.div`
 const TextContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  gap: 10px;
+`;
+
+const CauseText = styled.div`
+  font-size: 14px;
+  color: ${({ theme }) => theme.colors.GRAY_700};
+  margin-top: 10px;
 `;
