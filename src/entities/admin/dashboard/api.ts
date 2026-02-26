a5d71ae8f23b34af27431f300006c5a333ec01a6
김도예 <kmdy125@gmail.com>
2026-02-26 17:43:35 +0900

    feat: 대시보드 관련 API 및 쿼리 작성

---
+++ src/entities/admin/dashboard/api.ts
@@ -0,0 +1,34 @@
+import axiosInstance from '@/shared/config/axios';
+import {
+  GetDashboardBasicRes,
+  GetDashboardMembersRes,
+  GetDashboardPostRes,
+  GetDashboardReq,
+  GetDashboardTradeRes,
+} from '.';
+
+export const getDashboardBasic = async (): Promise<GetDashboardBasicRes> => {
+  const { data } = await axiosInstance.get('/statistics/basic');
+  return data;
+};
+
+export const getDashboardMembers = async (
+  params: GetDashboardReq,
+): Promise<GetDashboardMembersRes> => {
+  const { data } = await axiosInstance.get('/statistics/members', { params });
+  return data;
+};
+
+export const getDashboardPost = async (
+  params: GetDashboardReq,
+): Promise<GetDashboardPostRes> => {
+  const { data } = await axiosInstance.get('/statistics/post', { params });
+  return data;
+};
+
+export const getDashboardTrade = async (
+  params: GetDashboardReq,
+): Promise<GetDashboardTradeRes> => {
+  const { data } = await axiosInstance.get('/statistics/trade', { params });
+  return data;
+};
